a432671816b39ea7dc078d52973cd67624295a1f
Niklas Andersson <me@niklasandersson.net>
2018-01-25 13:59:25 +0100

    Add support for Doxygen documentation

--- project/src/include/some_function.hpp
+++ project/src/include/some_function.hpp
@@ -1,1 +1,17 @@
+/**
+ * @file some_function.hpp
+ * @brief This file contains some function
+ * @author Niklas Andersson
+ * @version 1.1
+ * @date 2018-01-25
+ */
+
+
+/**
+ * @brief This function does someting
+ *
+ * @param some_value This variable has some value
+ *
+ * @return This function returns some value
+ */
 int some_function(int some_value);

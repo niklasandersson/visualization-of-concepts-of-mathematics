e26e83046a28d5c7667af96502510b535951d033
Niklas Andersson <me@niklasandersson.net>
2018-02-07 16:34:34 +0100

    Add subdirectory

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
 {
   std::cout << "Hello World" << std::endl;
   std::cout << "What does some_function say? " << some_function(20) << std::endl;
-  std::cout <<GetResourcePath() << std::endl;
+  std::cout <<GetResourcePath("HEJ") << std::endl;
   return 0;
 }
